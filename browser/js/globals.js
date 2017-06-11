d8b74a12329fc171c479c5d3bf4ba89ccb5778f3
Solomon Omojola <omojolasolomon@gmail.com>
2017-06-11 22:57:12 +0200

    Parse links within messages and make them clickable to open in browser.

--- browser/js/globals.js
+++ browser/js/globals.js
@@ -1,7 +1,8 @@
 const electron = require('electron');
 const { Menu, MenuItem } = electron.remote
 const ipcRenderer = electron.ipcRenderer;
-const DUMMY_CHAT_ID = 'fake id'
+const DUMMY_CHAT_ID = 'fake id';
+const urlRegex = new RegExp(/(([a-z]+:\/\/)?(([a-z0-9\-]+\.)+([a-z]{2}|aero|arpa|biz|com|coop|edu|gov|info|int|jobs|mil|museum|name|nato|net|org|pro|travel|local|internal))(:[0-9]{1,5})?(\/[a-z0-9_\-\.~]+)*(\/([a-z0-9_\-\.]*)(\?[a-z0-9+_\-\.%=&amp;]*)?)?(#[a-zA-Z0-9!$&'()*+.=-_~:@/?]*)?)(\s+|$)/, 'i');
 
 window.chats = [];
 window.chatsHash = {};
