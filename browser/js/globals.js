9de5651809b3240e4e9d13ac4d99bd5a3b3ec4b1
Ifedapo Olarewaju <ifedapoolarewaju@gmail.com>
2017-09-25 00:58:39 +0100

    fix: match url protocols

--- browser/js/globals.js
+++ browser/js/globals.js
@@ -3,7 +3,7 @@ const { Menu, MenuItem } = electron.remote
 const ipcRenderer = electron.ipcRenderer;
 const DUMMY_CHAT_ID = 'fake id';
 const MSG_INPUT_SELECTOR = '.new-message form textarea';
-const URL_REGEX = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'i');
+const URL_REGEX = new RegExp(/(http:\/\/|https:\/\/)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/, 'i');
 
 window.chats = [];
 window.chatsHash = {};
