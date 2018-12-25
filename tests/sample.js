994edf557a65e919252e173a3a0a45cca835cb38
curtisy <lausklaus44@gmail.com>
2018-12-25 22:34:35 +0100

    Fix test build
    Whoops, I forgot to add the object descriptor..

--- tests/sample.js
+++ tests/sample.js
@@ -1,7 +1,7 @@
 const { assert } = intern.getPlugin('chai');
 const { registerSuite } = intern.getPlugin('interface.object');
 
-registerSuite({
+registerSuite('sample', {
     'tests': {
         'create new'() {
             assert.equal(1, 1, "Numbers don't equal");
