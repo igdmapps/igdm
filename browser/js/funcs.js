a8df57b6ef5087b982ddc70f0ccfd920965a9525
Russell <2600920+7urkm3n@users.noreply.github.com>
2021-05-28 10:19:25 -0400

    Fixes image loads (#1401)
    * fixes image loads
    
    * fixes image loads
    
    * version 3.0.3
    
    * removed unused comment
    
    * don't update version yet
    
    Co-authored-by: Ifedapo .A. Olarewaju <ifedapoolarewaju@gmail.com>

--- browser/js/funcs.js
+++ browser/js/funcs.js
@@ -346,7 +346,13 @@ function quoteText (text) {
 function setProfilePic () {
   const url = window.loggedInUser.profile_pic_url;
   const settingsButton = document.querySelector('.settings');
-  settingsButton.style.backgroundImage = `url(${url})`;
+
+  img = new Image;
+  img.crossOrigin = 'Anonymous';
+  img.src = url;
+  img.classList.add('thumb');
+  img.style.margin = '-1px 0 0 -15px';
+  settingsButton.appendChild(img);
 }
 
 function getLoadingGif () {
